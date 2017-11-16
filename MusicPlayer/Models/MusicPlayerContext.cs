7ecf8ee219c408bc0d6acde68c57c0cda13e4e50
Adrian Rożalski <adrianroz00@gmail.com>
2017-11-16 20:36:11 +0100

    Added AlbumsController

--- MusicPlayer/Models/MusicPlayerContext.cs
+++ MusicPlayer/Models/MusicPlayerContext.cs
@@ -20,5 +20,7 @@ namespace MusicPlayer.Models
         }
 
         public System.Data.Entity.DbSet<MusicPlayer.Models.Song> Songs { get; set; }
+
+        public System.Data.Entity.DbSet<MusicPlayer.Models.Album> Albums { get; set; }
     }
 }
